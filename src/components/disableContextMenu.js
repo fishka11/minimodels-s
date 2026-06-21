fbbdc35b9d91e47109c2acb6d3bf905b4081e24d
Rafal Piasnik <r.piasnik@gmail.com>
2026-06-21 11:19:38 +0200

    restrict oncontext menu alert onl to images and not studio pathname

--- src/components/disableContextMenu.js
+++ src/components/disableContextMenu.js
@@ -1,20 +1,36 @@
 "use client";
 
 import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 export default function DisableContextMenu() {
+  const pathname = usePathname();
+
   useEffect(() => {
+    // nie blokujemy w /studio
+    if (pathname.startsWith("/studio")) return;
+
     const handler = (e) => {
-      e.preventDefault();
-      alert("ZAKAZ KOPIOWANIA. WSZYSTKIE PRAWA ZASTRZEZONE. MINIMODELS.COM.PL");
+      const target = e.target;
+
+      // 1) Zwykłe <img>
+      const isImg = target.tagName?.toLowerCase() === "img";
+
+      // 2) Element z background-image
+      const bgImage = window
+        .getComputedStyle(target)
+        .getPropertyValue("background-image");
+      const hasBackgroundImage = bgImage && bgImage !== "none";
+
+      if (isImg || hasBackgroundImage) {
+        e.preventDefault();
+        alert("Prawy przycisk myszy jest zablokowany na obrazkach");
+      }
     };
 
     document.addEventListener("contextmenu", handler);
-
-    return () => {
-      document.removeEventListener("contextmenu", handler);
-    };
-  }, []);
+    return () => document.removeEventListener("contextmenu", handler);
+  }, [pathname]);
 
   return null;
 }
